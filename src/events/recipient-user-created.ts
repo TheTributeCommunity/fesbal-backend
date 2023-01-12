0c9af540a7ba8b276928e6f4d25197f9faaac51c
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-12 10:58:00 +0100

    feat: Add RecipientUserCreated event

---
+++ src/events/recipient-user-created.ts
@@ -0,0 +1,20 @@
+import { Event } from '@boostercloud/framework-core'
+import { UUID } from '@boostercloud/framework-types'
+
+@Event
+export class RecipientUserCreated {
+  public constructor(
+    readonly recipientUserId: UUID,
+    readonly firstName: string,
+    readonly lastName: string,
+    readonly password: string,
+    readonly dateOfBirth: string,
+    readonly address: string,
+    readonly phone: number,
+    readonly familyMembersCount: number
+  ) {}
+
+  public entityID(): UUID {
+    return this.recipientUserId
+  }
+}
