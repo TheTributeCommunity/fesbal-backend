75b2f9f2414827e04958ac4cf05ad95664826315
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-13 16:59:14 +0100

    feat(auth): Add authorization with publicKey for local usage

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,16 @@
-import { Booster } from '@boostercloud/framework-core'
+import { Booster, PublicKeyTokenVerifier } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
+import * as fs from 'fs'
+import * as path from 'path'
+
+function publicKeyResolver(): Promise<string> {
+  return Promise.resolve(fs.readFileSync(path.join(__dirname, '..', '..', 'assets', 'certs', 'public.key'), 'utf8'))
+}
 
 Booster.configure('local', (config: BoosterConfig): void => {
   config.appName = 'fesbal-backend'
   config.providerPackage = '@boostercloud/framework-provider-local'
+  config.tokenVerifiers = [new PublicKeyTokenVerifier('fesbal', publicKeyResolver(), 'role')]
 })
 
 Booster.configure('production', (config: BoosterConfig): void => {
