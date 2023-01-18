b7cd638ef33c1edf17ee5e2a216344e96c267ce2
melinahernandeztheam <melinahernandez@theagilemonkeys.com>
2023-01-18 11:46:28 +0100

    fix(CreateRecUser): authorize all and throw an error if the user is not in FB

--- src/commands/create-recipient-user.ts
+++ src/commands/create-recipient-user.ts
@@ -4,6 +4,7 @@ import { RecipientUserCreated } from '../events/recipient-user-created'
 import { AuthService } from '../services/auth-service'
 import { TypeOfIdentityDocument } from '../common/type-of-identity-document'
 import { RecipientUserRole } from '../common/recipient-user-role'
+import { RecipientUserNotFoundInFirebaseError } from '../common/recipient-user-not-registered-in-firebase-error'
 
 @Command({
   authorize: 'all',
@@ -20,7 +21,13 @@ export class CreateRecipientUser {
   ) {}
 
   public static async handle(command: CreateRecipientUser, register: Register): Promise<void> {
-    await AuthService.setRole(register.currentUser?.claims.user_id as string, RecipientUserRole.UserRegistered)
+    await AuthService.setRole(register.currentUser?.claims.user_id as string, RecipientUserRole.UserRegistered).catch(
+      (error) => {
+        console.log(error)
+        throw new RecipientUserNotFoundInFirebaseError(command.recipientUserId)
+      }
+    )
+
     register.events(
       new RecipientUserCreated(
         command.recipientUserId,
