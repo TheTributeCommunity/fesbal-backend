8ee1c4ffc004e7f78be7423b8df5fd638e2f3a12
Santi <santi.gamo@gmail.com>
2023-04-01 13:36:33 +0200

    feat(rocket file uploads): add dependencies and configurations

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,13 +2,19 @@ import { Booster, JwksUriTokenVerifier } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import * as dotenv from 'dotenv'
 import { AuthService } from '../services/auth-service'
-import { UploadFileService } from '../services/upload-file-service'
+import { RocketFilesUserConfiguration } from '@boostercloud/rocket-file-uploads-types'
+import { BoosterRocketFiles } from '@boostercloud/rocket-file-uploads-core'
+
+const rocketFilesConfigurationDefault: RocketFilesUserConfiguration = {
+  storageName: 'fesbalst', // AWS S3 bucket name
+  containerName: '', // Not used in AWS
+  directories: ['derivations'], // Root directories for your files
+}
 
 Booster.configure('local', (config: BoosterConfig): void => {
   dotenv.config()
 
   AuthService.initialize()
-  UploadFileService.initialize()
 
   config.appName = 'fesbal-backend-local'
   config.providerPackage = '@boostercloud/framework-provider-local'
@@ -19,13 +25,13 @@ Booster.configure('local', (config: BoosterConfig): void => {
       'role'
     ),
   ]
+  config.rockets = [new BoosterRocketFiles(config, [rocketFilesConfigurationDefault]).rocketForLocal()]
 })
 
 Booster.configure('development', (config: BoosterConfig): void => {
   dotenv.config()
 
   AuthService.initialize()
-  UploadFileService.initialize()
 
   config.assets = ['.env']
   config.appName = 'fesbal-backend-dev'
@@ -37,4 +43,5 @@ Booster.configure('development', (config: BoosterConfig): void => {
       'role'
     ),
   ]
+  config.rockets = [new BoosterRocketFiles(config, [rocketFilesConfigurationDefault]).rocketForAWS()]
 })
