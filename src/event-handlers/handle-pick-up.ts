ca425df972c3eb02cc60a4eae12ee3f0f6b53e7f
Santi <santi.gamo@gmail.com>
2023-04-26 14:06:30 +0200

    feat: notify user to sign pick up

--- src/event-handlers/handle-pick-up.ts
+++ src/event-handlers/handle-pick-up.ts
@@ -1,11 +1,14 @@
 import { Booster, EventHandler } from '@boostercloud/framework-core'
-import { Register } from '@boostercloud/framework-types'
+import { Register, UUID } from '@boostercloud/framework-types'
 import { PickUpSubmitted } from '../events/pick-up/pick-up-submitted'
 import { PickUp } from '../entities/pick-up'
 import { SignRequested } from '../events/pick-up/sign-requested'
 import { PickUpSigned } from '../events/pick-up/pick-up-signed'
 import { RecipientPickUpDone } from '../events/pick-up/recipient-pick-up-done'
 import { EntityDeliveryDone } from '../events/pick-up/entity-delivery-done'
+import { Recipient } from '../entities/recipient'
+import { NewNotification } from '../events/notification/new-notification'
+import { RecipientNotified } from '../events/notification/recipient-notified'
 
 @EventHandler(PickUpSubmitted)
 export class HandlePickUpSubmitted {
@@ -15,7 +18,25 @@ export class HandlePickUpSubmitted {
       throw new Error('PickUp not found')
     }
 
+    const recipient = await Booster.entity(Recipient, pickUp.receiptId)
+    if (!recipient) {
+      throw new Error('Recipient not found')
+    }
+    const notificationId = UUID.generate()
     register.events(new SignRequested(pickUp.receiptId, event.pickUpId))
+    register.events(
+      new NewNotification(
+        notificationId,
+        recipient?.id,
+        'Tiene una entrega pendiente de firmar',
+        `Hola ${recipient?.firstName}!, Tienes una entrega pendiente de firmar. Por favor, accede a la aplicación para firmarla.`,
+        false,
+        new Date(),
+        undefined,
+        false
+      )
+    )
+    register.events(new RecipientNotified(recipient?.id, notificationId))
   }
 }
 
@@ -27,7 +48,9 @@ export class HandlePickUpSigned {
       throw new Error('PickUp not found')
     }
 
-    register.events(new RecipientPickUpDone(event.recipientId, event.pickUpId))
-    register.events(new EntityDeliveryDone(pickUp.entityId, event.pickUpId))
+    register.events(
+      new RecipientPickUpDone(event.recipientId, event.pickUpId),
+      new EntityDeliveryDone(pickUp.entityId, event.pickUpId)
+    )
   }
 }
--- src/events/notification/recipient-notified.ts
+++ src/events/notification/recipient-notified.ts
@@ -2,7 +2,7 @@ import { Event } from '@boostercloud/framework-core'
 import { UUID } from '@boostercloud/framework-types'
 
 @Event
-export class recipientNotified {
+export class RecipientNotified {
   public constructor(readonly recipientId: UUID, readonly notificationId: UUID) {}
 
   public entityID(): UUID {
